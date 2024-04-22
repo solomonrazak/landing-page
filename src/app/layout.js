e0ba3ef38f4585fc1e1582e7407ed4515f94fea1
solomonraza <solomonrazak99@gmsil.com>
2024-04-22 08:55:10 -0700

    Resolved merge conflicts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,8 +23,9 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </head>
       <body className={`${inter.className}`}>
-
+       <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import Header from "@/components/header/Header";
 export default function Home() {
   return (
     <main className="">
+      <Header />
       <Link href="/pages/projects">
         <p className="m-4 p-4 rounded-lg bg-blue-500 w-40 text-center text-white">
           Projects
