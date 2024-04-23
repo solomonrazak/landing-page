1b6c14e527755ebc857a568d82131755ba30a31c
Sumit Kumar <106050212+sumit957602@users.noreply.github.com>
2024-04-23 16:46:42 +0530

    fixed import problem

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
-import Projectpage from "./Pages/projects/page.jsx";
-import Services from "./Pages/services/page.jsx";
+import Projectpage from "./Pages/Projects/projectpage.jsx";
+import Services from "./Pages/services/servicespage.jsx";
 export default function Home() {
   return (
     <main className="">
