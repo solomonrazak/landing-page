ab65a55ed65354201500f005aed3bf80d6938f45
Tissu07 <tissupal07@gmail.com>
2024-04-25 21:10:16 +0530

    project component optimised

--- src/app/Pages/Project/page.jsx
+++ src/app/Pages/Project/page.jsx
@@ -5,118 +5,104 @@ import Image from "next/image";
 function Projectpage() {
   return (
     <div
-      className="mx-6 mt-10 sm:mx-40 md:mx-20 text-gray-200 md:mt-10 md:pb-[10rem]"
-      style={{
-        background:
-          "radial-gradient(44.48% 44.48% at 50% 50%, rgba(250, 117, 248, 0.26) 0%, rgba(250, 117, 248, 0) 100%)",
-      }}
+      className="mx-4 mt-10 sm:mx-40 md:mx-20 text-gray-200 md:mt-10 md:pb-[10rem]"
+      // style={{
+      //   background:
+      //     "radial-gradient(44.48% 44.48% at 50% 50%, rgba(250, 117, 248, 0.26) 0%, rgba(250, 117, 248, 0) 100%)",
+      // }}
     >
       <p className="text-center text-2xl font-bold lg:text-5xl pb-3 ">
         Projects Lorem, ipsum dolor.
       </p>
-      <p className="text-center mt-2 text-lg lg:text-2xl md:mx-40 md:mb-[-30px]">
+      <p className="text-center mt-2 text-lg lg:text-2xl md:mx-40 md:mb-[-30px] ">
         Lorem ipsum dolor sit amet, consectetur
-        <span className="hidden md:block md:mt-5 md:mb-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,{" "}
-        </span>
-        <span className="">quae.</span>
       </p>
 
       <div className="flex flex-col px-[10%] md:mt-20 md:px-[5%]">
-        <div className="mt-20 flex flex-col md:flex-row md:justify-between gap-[3rem]">
-          <div className="">
+        <div className="mt-20 flex flex-col md:flex-row md:justify-between ">
+          <div className="md:w-1/2">
             <p className="text-2xl lg:text-4xl">
               Lorem ipsum <br />
               dolor sit.
             </p>
-            <p className="my-4 text-lg lg:text-xl">
-              Lorem ipsum dolor sit, amet consectetur adipisicing
+            <p className="my-8 text-lg lg:text-xl  ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Pariatur, quisquam.
+            </p>
+            <p className=" text-lg lg:text-xl hidden md:block ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum
+              dolor sit
             </p>
           </div>
 
-          <div className="">
+          <div className="relative">
             <Image
-              className="object-cover h-[12rem] md:h-[160%] mx-auto"
+              className="object-cover h-[12rem] md:h-[140%] mx-auto "
               src="/images/project1.jpg"
               alt=""
               priority={true}
               width={500}
               height={500}
             />
-            {/* <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[160%]"></div> */}
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
           </div>
         </div>
 
-        <div className="mt-20 md:mt-40 flex flex-col md:flex-row-reverse md:justify-between gap-[3rem] md:pt-[5rem]">
-          <div className="md:pl-[6rem]">
-            <p className="text-2xl lg:text-3xl">
+        <div className="mt-20 md:mt-60 flex flex-col md:flex-row-reverse md:justify-between ">
+          <div className="md:w-1/2">
+            <p className="text-2xl lg:text-4xl">
               Lorem ipsum <br />
               dolor sit.
             </p>
-            <p className="my-4 text-lg lg:text-xl">
-              Lorem ipsum dolor sit, amet consectetur adipisicing
+            <p className="my-8 text-lg lg:text-xl  ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Pariatur, quisquam.
             </p>
-            <p>elit. Blanditiis, quis! Lorem ipsum dolor sit amet.</p>
+            <p className=" text-lg lg:text-xl hidden md:block ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum
+              dolor sit
+            </p>
           </div>
 
-          <div className="">
+          <div className="mt-4 relative">
             <Image
-              className="object-cover h-[12rem] md:h-[160%]"
+              className="object-cover h-[12rem] md:h-[140%] mx-auto "
               src="/images/project1.jpg"
               alt=""
               priority={true}
               width={500}
               height={500}
             />
-            {/* <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[160%]"></div> */}
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
           </div>
         </div>
 
-        <div className="mt-20 lg:mt-40 flex flex-col md:flex-row md:justify-between gap-[3rem] md:pt-[5rem]">
-          <div className="">
+        <div className="mt-20 md:mt-60 flex flex-col md:flex-row md:justify-between ">
+          <div className="md:w-1/2">
             <p className="text-2xl lg:text-4xl">
               Lorem ipsum <br />
               dolor sit.
             </p>
-            <p className="my-4 text-lg lg:text-xl">
-              Lorem ipsum dolor sit, amet consectetur adipisicing
+            <p className="my-8 text-lg lg:text-xl  ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Pariatur, quisquam.
             </p>
-            <p>elit. Blanditiis, quis! Lorem ipsum dolor sit amet.</p>
+            <p className=" text-lg lg:text-xl  hidden md:block">
+              Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum
+              dolor sit
+            </p>
           </div>
 
-          <div className="">
+          <div className="relative">
             <Image
-              className="object-cover h-[12rem] md:h-[160%]"
+              className="object-cover h-[12rem] md:h-[140%] mx-auto "
               src="/images/project1.jpg"
               alt=""
               priority={true}
               width={500}
               height={500}
             />
-          </div>
-        </div>
-
-        <div className="mt-20 lg:mt-40 flex flex-col lg:flex-row gap-[3rem] lg:ml-20 md:hidden">
-          <div className="lg:w-1/2">
-            <p className="text-2xl lg:text-4xl">
-              Lorem ipsum <br />
-              dolor sit.
-            </p>
-            <p className="my-4 text-lg lg:text-xl">
-              Lorem ipsum dolor sit, amet consectetur adipisicing
-            </p>
-            <p>elit. Blanditiis, quis! Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="">
-            <Image
-              class="object-cover h-[12rem] w-full md:h-[160%]"
-              src="/images/project1.jpg"
-              alt=""
-              width={500}
-              height={500}
-            />
-            {/* <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[160%]"></div> */}
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
           </div>
         </div>
       </div>
