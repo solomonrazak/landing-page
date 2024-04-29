710a927b18a2abe31a4225491d0bb63fdfff0943
praishere <97793061+praishere@users.noreply.github.com>
2024-04-29 11:32:51 +0530

    Latest Project update

--- src/app/Pages/Project/page.jsx
+++ src/app/Pages/Project/page.jsx
@@ -3,22 +3,9 @@ import Image from "next/image";
 
 function Projectpage() {
   return (
-<<<<<<< Updated upstream
-    <div
-      className="mx-4 mt-10 sm:mx-40 md:mx-20 text-gray-200 md:mt-10 md:pb-[10rem]"
-      style={{
-        background:
-          "radial-gradient(44.48% 34.48% at 60% 43%, rgba(176, 117, 248, 0.15) 0%, rgba(250, 117, 248, 0) 80%)",
-          opacity: "0.85",
-      }}
-    >
-      <p className="md:text-center mx-4 text-3xl font-bold lg:text-5xl pb-3 ">
-        Projects Lorem, ipsum dolor.
-=======
     <div className="mx-4 mt-10 sm:mx-40 md:mx-20 text-gray-200 md:mt-10 md:pb-[10rem]">
       <p className="md:text-center mx-4 text-3xl font-bold lg:text-5xl pb-3">
         Projects
->>>>>>> Stashed changes
       </p>
       <p className="md:text-center mt-2 mx-4 text-lg lg:text-2xl md:mx-40 md:mb-[-30px]">
         Awesome Projects made by our developers
@@ -28,27 +15,25 @@ function Projectpage() {
         <div className="mt-20 flex flex-col md:flex-row md:justify-between">
           <div className="md:w-1/2">
             <p className="text-2xl lg:text-4xl font-semibold">
-              PetCare<br />
+              PetCare
             </p>
             <p className="my-8 text-lg lg:text-xl">
               Welcome to Pet Care, your premier destination for all things pets! From helpful 
               care guides to heartwarming adoption stories. We&apos;re here to celebrate the joy and 
               companionship that our furry friends bring.
             </p>
-            <p className="text-lg lg:text-xl hidden md:block">
-            </p>
             <a href="https://petcare-tau.vercel.app" target="_blank" rel="noopener noreferrer">
-          <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
-         Click
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
-        </button>
-         </a>
+              <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
+                Click
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
+              </button>
+            </a>
           </div>
 
           <div className="relative">
             <Image
               className="object-cover h-[12rem] md:h-[140%] mx-auto"
-              src="/images1/Projectt.JPG" // Adjusted path
+              src="/images/Projectt.JPG"
               alt=""
               priority={true}
               width={500}
@@ -61,25 +46,23 @@ function Projectpage() {
         <div className="mt-20 md:mt-60 flex flex-col md:flex-row-reverse md:justify-between">
           <div className="md:w-1/2">
             <p className="text-2xl lg:text-4xl font-semibold">
-              Bid-Bazar<br />
+              Bid-Bazar
             </p>
             <p className="my-8 text-lg lg:text-xl">
               A platform where buyers and sellers can congregate for the purpose of seamless bidding and selling across a wide spectrum of items.
             </p>
-            <p className="text-lg lg:text-xl hidden md:block">
-            </p>
-            <a href="https://bid-bazaar-frontend.vercel.app/p" target="_blank" rel="noopener noreferrer">
-          <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
-         Click
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
-        </button>
-         </a>
+            <a href="https://bid-bazaar-frontend.vercel.app" target="_blank" rel="noopener noreferrer">
+              <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
+                Click
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
+              </button>
+            </a>
           </div>
 
           <div className="mt-4 relative">
             <Image
               className="object-cover h-[12rem] md:h-[140%] mx-auto"
-              src="/images1/Prabhat_Website.jpg" // Adjusted path
+              src="/images/Prabhat_Website.jpg"
               alt=""
               priority={true}
               width={500}
@@ -92,26 +75,23 @@ function Projectpage() {
         <div className="mt-20 md:mt-60 flex flex-col md:flex-row md:justify-between">
           <div className="md:w-1/2">
             <p className="text-2xl lg:text-4xl font-semibold">
-              LeashPay<br />
+              LeashPay
             </p>
             <p className="my-8 text-lg lg:text-xl">
               Regardless of the size of your business, LeashPay offers everything software and API needed to build a world-class payments experiences.
             </p>
-            <p className="text-lg lg:text-xl hidden md:block">
-            </p>
             <a href="https://leashpay.netlify.app" target="_blank" rel="noopener noreferrer">
-          <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
-         Click
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
-        </button>
-         </a>
+              <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
+                Click
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
+              </button>
+            </a>
           </div>
 
-          
           <div className="relative">
             <Image
               className="object-cover h-[12rem] md:h-[140%] mx-auto"
-              src="/images1/zack_website.jpg" // Adjusted path
+              src="/images/zack_website.jpg"
               alt=""
               priority={true}
               width={500}
@@ -124,26 +104,23 @@ function Projectpage() {
         <div className="mt-20 md:mt-60 flex flex-col md:flex-row-reverse md:justify-between">
           <div className="md:w-1/2">
             <p className="text-2xl lg:text-4xl font-semibold">
-              Nike Store<br />
+              Nike Store
             </p>
             <p className="my-8 text-lg lg:text-xl">
               This is a full fledged Nike store that helps the consumer to buy the products they want
             </p>
-            <p className="text-lg lg:text-xl hidden md:block">
-            </p>
-            <a href="https://nike-clone-three.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
-         Click
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
-        </button>
-         </a>
-
+            <a href="https://nike-clone-three.vercel.app" target="_blank" rel="noopener noreferrer">
+              <button className="mt-4 relative bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600">
+                Click
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black lg:h-[140%]"></div>
+              </button>
+            </a>
           </div>
 
           <div className="mt-4 relative">
             <Image
               className="object-cover h-[12rem] md:h-[140%] mx-auto"
-              src="/images1/Sumit_new.jpg"
+              src="/images/Sumit_new.jpg"
               alt=""
               priority={true}
               width={500}
