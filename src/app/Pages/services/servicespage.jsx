f3186dbc184c96a1e3a49d58bfc71f0a48067c99
Tissu07 <tissupal07@gmail.com>
2024-05-11 13:25:00 +0530

    service content added

--- src/app/Pages/services/servicespage.jsx
+++ src/app/Pages/services/servicespage.jsx
@@ -12,11 +12,46 @@ const cloudIcon = (
   </svg>
 );
 
+const TechnicalSupportIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    className="service-icon w-4 md:w-6"
+    width="40"
+    height="40"
+  >
+    <path
+      fill="currentColor"
+      d="M256 0C114.84 0 0 114.83 0 256s114.84 256 256 256 256-114.83 256-256S397.16 0 256 0zm0 464c-114.88 0-208-93.12-208-208S141.12 48 256 48s208 93.12 208 208-93.12 208-208 208zM256 96c-52.94 0-96 43.06-96 96 0 25.76 10.08 49.74 28.39 67.96L64 368h64v32h64v-32h32v32h64v-32h64v-64.03C341.92 317.76 352 293.78 352 268c0-52.94-43.06-96-96-96zm32 172v24h-64v-24c-32.77 0-59.56-24.73-63.69-56H180c-26.47 0-48-21.53-48-48s21.53-48 48-48h44.31c4.14-31.27 30.93-56 63.69-56v-24h64v24c32.77 0 59.56 24.73 63.69 56H332c26.47 0 48 21.53 48 48s-21.53 48-48 48h-44.31c-4.14 31.27-30.93 56-63.69 56z"
+    ></path>
+  </svg>
+);
+
+const QualityAssuranceIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    className="service-icon  w-4 md:w-6"
+  >
+    <path d="M256 0c4.6 0 9.2 1 13.4 2.9L457.7 82.8c22 9.3 38.4 31 38.3 57.2c-.5 99.2-41.3 280.7-213.6 363.2c-16.7 8-36.1 8-52.8 0C57.3 420.7 16.5 239.2 16 140c-.1-26.2 16.3-47.9 38.3-57.2L242.7 2.9C246.8 1 251.4 0 256 0zm0 66.8V444.8C394 378 431.1 230.1 432 141.4L256 66.8l0 0z" />
+  </svg>
+);
+
+const SolutionIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    className="service-icon w-4 md:w-6"
+  >
+    <path d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
+  </svg>
+);
+
 const mobileIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 384 512"
-    className="service-icon  w-4 md:w-6"
+    className="service-icon w-4 md:w-6"
   >
     <path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z" />
   </svg>
@@ -75,11 +110,15 @@ const Services = () => {
                 </div>
               </div>
               <div>
-                <h1 className="text-xl font-semibold md:text-3xl">heading</h1>
+                <h1 className="text-xl font-semibold md:text-3xl">
+                  Web Development:
+                </h1>
                 <p className="text-lg mt-2 md:text-xl md:leading-relaxed">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae tenetur Lorem ipsum dolor sit amet consectetur
-                  adipisicing.
+                  Step into the digital realm with our seasoned team of web
+                  developers. We specialize in crafting bespoke websites
+                  tailored to your exact specifications. From seamless
+                  functionality to captivating design, we ensure your online
+                  presence stands out from the crowd.
                 </p>
               </div>
             </div>
@@ -90,11 +129,14 @@ const Services = () => {
                 </div>
               </div>
               <div>
-                <h1 className="text-xl font-semibold md:text-3xl">heading</h1>
+                <h1 className="text-xl font-semibold md:text-3xl">
+                  UI/UX Design:
+                </h1>
                 <p className="text-lg mt-2 md:text-xl md:leading-relaxed">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae tenetur Lorem ipsum dolor sit amet consectetur
-                  adipisicing.
+                  Elevate your digital brand with our premium UI/UX design
+                  services. Focused on user-centric principles, we meticulously
+                  craft interfaces that not only look stunning but also enhance
+                  user engagement and drive conversion rates.
                 </p>
               </div>
             </div>
@@ -105,11 +147,15 @@ const Services = () => {
                 </div>
               </div>
               <div>
-                <h1 className="text-xl font-semibold md:text-3xl">heading</h1>
+                <h1 className="text-xl font-semibold md:text-3xl">
+                  Mobile App Development:
+                </h1>
                 <p className="text-lg mt-2 md:text-xl md:leading-relaxed">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae tenetur Lorem ipsum dolor sit amet consectetur
-                  adipisicing.
+                  Embrace the mobile revolution with our cutting-edge app
+                  development services. Whether it's iOS, Android, or
+                  cross-platform solutions, our experts are dedicated to
+                  creating intuitive and engaging mobile applications that
+                  resonate with your audience.
                 </p>
               </div>
             </div>
@@ -119,45 +165,57 @@ const Services = () => {
             <div className="flex flex-row text-white gap-3 mt-8 md:mt-16">
               <div className="">
                 <div className=" border-2 w-7 md:w-12 border-white  px-1 md:px-3 md:py-2 rounded-md">
-                  {cloudIcon}
+                  {QualityAssuranceIcon}
                 </div>
               </div>
               <div>
-                <h1 className="text-xl font-semibold md:text-3xl">heading</h1>
+                <h1 className="text-xl font-semibold md:text-3xl">
+                  Quality Assurance:
+                </h1>
                 <p className="text-lg mt-2 md:text-xl md:leading-relaxed">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae tenetur Lorem ipsum dolor sit amet consectetur
-                  adipisicing.
+                  Ensure the integrity of your software products with our
+                  rigorous quality assurance processes. Our dedicated QA team
+                  meticulously tests and refines every aspect of your software,
+                  guaranteeing a seamless user experience and minimizing
+                  disruptions
                 </p>
               </div>
             </div>
             <div className="flex flex-row text-white gap-3 mt-8 md:mt-16">
               <div className="">
                 <div className=" border-2 w-7 md:w-12 border-white  px-1 md:px-3 md:py-2 rounded-md">
-                  {webIcon}
+                  {TechnicalSupportIcon}
                 </div>
               </div>
               <div>
-                <h1 className="text-xl font-semibold md:text-3xl">heading</h1>
+                <h1 className="text-xl font-semibold md:text-3xl">
+                  Technical Support:
+                </h1>
                 <p className="text-lg mt-2 md:text-xl md:leading-relaxed">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae tenetur Lorem ipsum dolor sit amet consectetur
-                  adipisicing.
+                  Experience peace of mind with our responsive technical support
+                  services. Whether it's troubleshooting, maintenance, or
+                  ongoing assistance, our dedicated team is committed to
+                  providing prompt and reliable support to keep your operations
+                  running smoothly.
                 </p>
               </div>
             </div>
             <div className="flex flex-row text-white gap-3 mt-8 md:mt-16">
               <div className="">
                 <div className=" border-2 w-7 md:w-12 border-white  px-1 md:px-3 md:py-2 rounded-md">
-                  {webIcon}
+                  {SolutionIcon}
                 </div>
               </div>
               <div>
-                <h1 className="text-xl font-semibold md:text-3xl">heading</h1>
+                <h1 className="text-xl font-semibold md:text-3xl">
+                  Software Consultation:
+                </h1>
                 <p className="text-lg mt-2 md:text-xl md:leading-relaxed">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae tenetur Lorem ipsum dolor sit amet consectetur
-                  adipisicing.
+                  Navigate the complexities of software development with
+                  confidence. Our seasoned consultants offer invaluable insights
+                  and strategic guidance to streamline your projects from
+                  conception to execution, ensuring optimal performance and
+                  efficiency.
                 </p>
               </div>
             </div>
